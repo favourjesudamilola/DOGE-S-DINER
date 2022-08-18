97e2b86d03ea24d6c58c5ddaa76604886ac1b3a4
Oluseun Dami <oluseundamilola@gmail.com>
2022-08-18 13:05:43 +0100

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,10 +13,13 @@ import { ProgressComponent } from './progress/progress.component';
 
 const routes: Routes = [
   {path:"", component:HomeComponent},
+  
   //shows product in the home component
   //to wrok import activated route from angular route inside the homecomp.ts
   // {path: 'search/:searchTerm', component:SearchComponent},
+
   {path: 'search/:searchTerm', component:HomeComponent},
+
   // can create a component that allows the search rout to it.
   {path: 'food/:id', component:FoodPageComponent},
   {path: 'history', component:HistoryComponent},
