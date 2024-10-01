e2e64a9a3e34f1e7659e5a37dff5348809176aa9
unknown <jesudamilola2000@gmail.com>
2024-10-01 14:42:54 +0100

    changes

--- src/app/shared/model/Food.ts
+++ src/app/shared/model/Food.ts
@@ -1,9 +1,6 @@
 //export makes the class accesible from outside food.ts
 //the exclamation marks shows the id of food is compulsory
 export class Food{
-    // filter(arg0: (cards: { name: any; searchTerm: string; }) => boolean) {
-    //   throw new Error('Method not implemented.');
-    // }
     id!: number;
     name!: string;
     price!: number;
